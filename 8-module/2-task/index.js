24ba0dcfc23001f02002217d7f960bd107d7e824
dagda25 <dagda25@gmail.com>
2020-05-13 09:55:09 +0300

    fix

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -8,24 +8,17 @@ export default class ProductGrid {
     this.products = products;
     this.render(products);
     this.filters = {};
-    /*let filters = {
-      noNuts: true, // true/false
-      vegeterianOnly: true, // true/false
-      maxSpiciness: 3, // числа от 0 до 4
-      category: 'soups' // уникальный идентификатор категории товара
-    };
-    this.updateFilter(filters);*/
   }
 
   render(products) {
     let inner = document.createElement('div');
     inner.classList.add("products-grid__inner");
+
     products.forEach((el) => {
       let card =  new ProductCard(el);
-
       inner.append(card.elem);
-
     });
+    
     this.elem.append(inner);
   }
 
@@ -46,16 +39,15 @@ export default class ProductGrid {
       if (!filters.category) return true;
       return filters.category === el.category;
     });
-    this.elem.innerHTML = "";
 
+    this.elem.innerHTML = "";
     let inner = document.createElement('div');
     inner.classList.add("products-grid__inner");
     filteredProducts.forEach((el) => {
       let card =  new ProductCard(el);
-
       inner.append(card.elem);
-
     });
+
     this.elem.append(inner);
   }
 
